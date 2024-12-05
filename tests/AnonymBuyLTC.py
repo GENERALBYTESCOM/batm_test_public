eb90c35d8164006a1562916da9a26088b07856dd
jurbanek <jindrich.urbanek@generalbytes.com>
2024-12-05 11:35:55 +0100

    BATM-6923: Setup Pylint and Black Formatting for Testing Repository
    - Configured Pylint to enforce camel case for variable and function naming.
    - Disabled irrelevant or nonsensical rules to streamline linting.
    - Separated Pylint rules into a dedicated `.pylintrc` file.
    - Integrated Black as a formatting tool for consistent code style.

--- tests/AnonymBuyLTC.py
+++ tests/AnonymBuyLTC.py
@@ -1,53 +1,72 @@
 import sys
-from sikuli import *
+from sikuli import wait, click, find, type, Pattern, has, waitVanish
 
 WAIT_TIMEOUT = 7
 BTC_DESTINATION_ADDRESS = "1GBjMVQputwgRkCJ9zK2u9KJVkcEnFXi7z"
 
+
 def assertExists(pattern, name):
-    sys.stdout.write("Assert '" + name + "' exists..."),
-    if (has(pattern, WAIT_TIMEOUT)):
+    sys.stdout.write("Assert '" + name + "' exists...")
+    if has(pattern, WAIT_TIMEOUT):
         sys.stdout.write("OK")
     else:
         sys.stdout.write("FAIL")
-        exit(1)
+        sys.exit()
     sys.stdout.flush()
+
 
 def assertClick(pattern, name):
-    sys.stdout.write("Click on '" + name + "'..."),
-    if (click(pattern)):
+    sys.stdout.write("Click on '" + name + "'...")
+    if click(pattern):
         sys.stdout.write("OK")
     else:
         sys.stdout.write("FAIL")
-        exit(1)
+        sys.exit()
     sys.stdout.flush()
 
+
 def openBanknoteDropdown():
-    if (has(Pattern("1722425469678.png").similar(0.71))):
-        assertClick(Pattern("1722425469678.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 100")
-    elif (has(Pattern("1722427265172.png").similar(0.95))):
-        assertClick(Pattern("1722427265172.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 200")
-    elif (has(Pattern("1722427299365.png").similar(0.95))):
-        assertClick(Pattern("1722427299365.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 500")
-    elif (has(Pattern("1722427411838.png").similar(0.95))):
-        assertClick(Pattern("1722427411838.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 1000")
-    elif (has(Pattern("1722427462571.png").similar(0.95))):
-        assertClick(Pattern("1722427462571.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 2000")
-    elif (has(Pattern("1722427510166.png").similar(0.95))):
-        assertClick(Pattern("1722427510166.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 5000")
+    if has(Pattern("1722425469678.png").similar(0.71)):
+        assertClick(
+            Pattern("1722425469678.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 100"
+        )
+    elif has(Pattern("1722427265172.png").similar(0.95)):
+        assertClick(
+            Pattern("1722427265172.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 200"
+        )
+    elif has(Pattern("1722427299365.png").similar(0.95)):
+        assertClick(
+            Pattern("1722427299365.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 500"
+        )
+    elif has(Pattern("1722427411838.png").similar(0.95)):
+        assertClick(
+            Pattern("1722427411838.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 1000"
+        )
+    elif has(Pattern("1722427462571.png").similar(0.95)):
+        assertClick(
+            Pattern("1722427462571.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 2000"
+        )
+    elif has(Pattern("1722427510166.png").similar(0.95)):
+        assertClick(
+            Pattern("1722427510166.png").similar(0.95), "OPEN BANKNOTES DROPDOWN 5000"
+        )
     else:
         sys.stdout.write("CANNOT OPEN DROPDOWN")
-        exit(1)
-    sys.stdout.flush()    
+        sys.exit()
+    sys.stdout.flush()
 
-if (has("1727773475891.png")):
-   assertClick("1727775827520.png", "SCREENSAVER")
+
+if has("1727773475891.png"):
+    assertClick("1727775827520.png", "SCREENSAVER")
 assertExists("1728304602566.png", "LTC LOGO")
 assertClick("1728304616977.png", "LTC LOGO")
 assertExists("1727773811641.png", "BUY BUTTON EXIST")
 assertClick("1727774023585.png", "BUY BUTTON")
-wait("1727774440040.png",  WAIT_TIMEOUT)
-assertExists("1727774440040.png", "WHERE DO YOU WANT TO SEND PURCHASED CRYPTO CURRENCY? TEXT EXIST")
+wait("1727774440040.png", WAIT_TIMEOUT)
+assertExists(
+    "1727774440040.png",
+    "WHERE DO YOU WANT TO SEND PURCHASED CRYPTO CURRENCY? TEXT EXIST",
+)
 assertClick("1727774055016.png", "SEND COINS TO MY WALLET BUTTON")
 assertExists("1728054995323.png", "PEP QUESTION TEXT EXIST")
 assertClick("1727774141922.png", "NO BUTTON")
@@ -70,7 +89,7 @@ find("1727954109388.png")
 assertClick("1727955584803.png", "CLICK INPUT LINE")
 type("aa@gg.com")
 assertClick("1727954348111.png", "SEND TEXT INPUT BUTTON")
-wait("1728045640206.png", WAIT_TIMEOUT) 
+wait("1728045640206.png", WAIT_TIMEOUT)
 assertClick("1728039389040.png", "DONE BUTTON")
 assertExists("1728039518831.png", "ONE TIME PASSWORD TEXT EXIST")
 assertClick("1728039530360.png", "OK BUTTON")
@@ -82,7 +101,7 @@ find("1727954109388.png")
 assertClick("1727955584803.png", "CLICK INPUT")
 type("123")
 assertClick("1727954348111.png", "SEND TEXT INPUT BUTTON")
-wait("1727954667429.png", WAIT_TIMEOUT) 
+wait("1727954667429.png", WAIT_TIMEOUT)
 assertExists("1727954277029.png", "ENTER DISCOUNT CODE LOGO")
 assertClick("1727954257108.png", "OK BUTTON")
 waitVanish("1727955164721.png", WAIT_TIMEOUT)
@@ -92,16 +111,16 @@ find("1727790898506.png")
 assertClick("1727791300173.png", "CLICK DROPDOWN")
 assertExists("1727784530186.png", "INSERTED VALUE = 100 CZK")
 assertClick("1727791315299.png", "INSERT BANKNOTE 100 CZK")
+
+
 def openBanknoteDropdown():
-    if (has("1727792164614.png")):
+    if has("1727792164614.png"):
         assertClick("1727792215829.png", "SELECT 100 CZK")
+
+
 assertClick("1727784547352.png", "INSERT BANKNOTE 100 CZK")
 assertExists("1727784268519.png", "INSERT CASH LOGO")
-wait("1728307044742.png", WAIT_TIMEOUT) 
+wait("1728307044742.png", WAIT_TIMEOUT)
 assertClick("1728307056118.png", "BUY LTC BUTTON")
 assertExists("1728305846284.png", "PROCESSING TRANSACTION COMPLETED TEXT EXIST")
 assertClick("1728056816610.png", "DONE BUTTON")
-
-
-
-
