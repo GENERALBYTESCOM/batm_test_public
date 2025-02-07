79e389a7667a4ecb71dc56b08e76367ff4c8313a
asaginayeva <assemgul.saginayeva@generalbytes.com>
2025-02-07 13:45:32 +0100

    updated AnonymBuyLTC.py

--- tests/AnonymBuyLTC.py
+++ tests/AnonymBuyLTC.py
@@ -1,5 +1,5 @@
 import sys
-from sikuli import *
+from sikuli import wait, click, find, type, Pattern, has, waitVanish
 
 WAIT_TIMEOUT = 7
 BTC_DESTINATION_ADDRESS = "1GBjMVQputwgRkCJ9zK2u9KJVkcEnFXi7z"
