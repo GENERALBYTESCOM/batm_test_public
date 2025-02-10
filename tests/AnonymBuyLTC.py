f33c790005b380c12da022181b4280d0fe4137f3
asaginayeva <assemgul.saginayeva@generalbytes.com>
2025-02-10 11:38:25 +0100

    updated screenshots

--- tests/AnonymBuyLTC.py
+++ tests/AnonymBuyLTC.py
@@ -24,66 +24,92 @@ def assertClick(pattern, name):
     sys.stdout.flush()
 
 
-if has("tests/screenshots/1724847707672.png"):
-    assertClick("tests/screenshots/1727773516372.png", "SCREENSAVER")
-assertExists("1738942971358.png", "LTC LOGO")
-assertClick("1738942971358.png", "LTC LOGO")
-assertExists("1738943148647.png", "BUY BUTTON EXIST")
-assertClick("1738943148647.png", "BUY BUTTON")
-wait("1738943262351.png", WAIT_TIMEOUT)
+if has("tests/screenshots/main_screen.png"):
+    assertClick("tests/screenshots/BTC_logo.png", "SCREENSAVER")
+assertExists("tests/screenshots/LTC_logo.png", "LTC LOGO")
+assertClick("tests/screenshots/LTC_logo.png", "LTC LOGO")
+assertExists("tests/screenshots/BUY_button.png", "BUY BUTTON EXIST")
+assertClick("tests/screenshots/BUY_button.png", "BUY BUTTON")
+wait("tests/screenshots/where_do_you_want_to_send_crypto_text.png", WAIT_TIMEOUT)
 assertExists(
-    "1738943262351.png",
+    "tests/screenshots/where_do_you_want_to_send_crypto_text.png",
     "WHERE DO YOU WANT TO SEND PURCHASED CRYPTO CURRENCY? TEXT EXIST",
 )
-assertClick("1738943373770.png", "SEND COINS TO MY WALLET BUTTON")
-assertExists("1738943408415.png", "ARE YOU A POLITICALLY EXPOSED PERSon TEXT EXIST")
-assertClick("1727774141922.png", "NO BUTTON")
-assertExists("1738943767931.png", "PRIVACY POLICY TEXT EXIST")
-assertClick("1738943780180.png", "I AGREE BUTTON")
-assertExists("1738944041565.png", "CHOOSE CASH LIMIT TEXT EXIST")
-assertClick("1738944025946.png", "UP TO 300 CZK BUTTON")
-assertExists("1738944218917.png", "DON'T HAVE WALLET? TEXT EXIST")
-assertClick("1738944218917.png", "DON'T HAVE WALLET BUTTON")
-assertExists("1738944500278.png", "DON'T HAVE A WALLET? TEXT EXIST")
-assertClick("1738944618727.png", "SEND ALTCOIN BY EMAIL BUTTON")
-wait("1738944859476.png", WAIT_TIMEOUT)
-assertClick("1738944887044.png", "INPUT LINE")
-find("1738945177975.png")
-assertClick("1727955584803.png", "CLICK INPUT LINE")
+assertClick(
+    "tests/screenshots/send_coins_to_my_wallet_button.png",
+    "SEND COINS TO MY WALLET BUTTON",
+)
+assertExists(
+    "tests/screenshots/are_you_politically_exposed_person_text.png",
+    "ARE YOU A POLITICALLY EXPOSED PERSon TEXT EXIST",
+)
+assertClick("tests/screenshots/NO_button.png", "NO BUTTON")
+assertExists("tests/screenshots/privacy_policy_button.png", "PRIVACY POLICY TEXT EXIST")
+assertClick("tests/screenshots/i_agree_button.png", "I AGREE BUTTON")
+assertExists(
+    "tests/screenshots/choose_cash_limit_text.png", "CHOOSE CASH LIMIT TEXT EXIST"
+)
+assertClick("tests/screenshots/up_to_300_CZK.png", "UP TO 300 CZK BUTTON")
+assertExists(
+    "tests/screenshots/don't_have_a_wallet_button.png", "DON'T HAVE WALLET? TEXT EXIST"
+)
+assertClick(
+    "tests/screenshots/don't_have_a_wallet_button.png", "DON'T HAVE WALLET BUTTON"
+)
+assertExists(
+    "tests/screenshots/don't_have_a_wallet_text.png", "DON'T HAVE A WALLET? TEXT EXIST"
+)
+assertClick(
+    "tests/screenshots/send_altcoin_by_email_button.png", "SEND ALTCOIN BY EMAIL BUTTON"
+)
+wait("tests/screenshots/your_email_text.png", WAIT_TIMEOUT)
+assertClick("tests/screenshots/input_line_for_email.png", "INPUT LINE")
+find("tests/screenshots/send_text_input_button.png")
+assertClick("tests/screenshots/send_text_input_field.png", "CLICK INPUT LINE")
 type("aa@gg.com")
-assertClick("1738945177975.png", "SEND TEXT INPUT BUTTON")
-wait("1738945655953.png", WAIT_TIMEOUT)
-assertClick("1738945655953.png", "DONE BUTTON")
-wait("1738946082323.png", WAIT_TIMEOUT)
-assertExists("1738946082323.png", "ONE TIME PASSWORD TEXT EXIST")
-assertClick("1738945878928.png", "OK BUTTON")
-wait("1738946287704.png", WAIT_TIMEOUT)
-assertExists("1738946502249.png", "DISCOUNT INPUT TEXT EXIST")
-assertClick("1738946513153.png", "ENTER DISCOUNT CODE BUTTON")
-wait("1738946538905.png", WAIT_TIMEOUT)
-find("1738945177975.png")
-assertClick("1727955584803.png", "CLICK INPUT")
+assertClick("tests/screenshots/send_text_input_button.png", "SEND TEXT INPUT BUTTON")
+wait("tests/screenshots/DONE_button.png", WAIT_TIMEOUT)
+assertClick("tests/screenshots/DONE_button.png", "DONE BUTTON")
+wait("tests/screenshots/one_time_password_text.png", WAIT_TIMEOUT)
+assertExists(
+    "tests/screenshots/one_time_password_text.png", "ONE TIME PASSWORD TEXT EXIST"
+)
+assertClick("tests/screenshots/OK_button.png", "OK BUTTON")
+wait("tests/screenshots/insert_cash_text.png", WAIT_TIMEOUT)
+assertExists("tests/screenshots/discount_text.png", "DISCOUNT INPUT TEXT EXIST")
+assertClick(
+    "tests/screenshots/enter_discount_code_button.png", "ENTER DISCOUNT CODE BUTTON"
+)
+wait("tests/screenshots/enter_discount_code_text.png", WAIT_TIMEOUT)
+find("tests/screenshots/send_text_input_button.png")
+assertClick("tests/screenshots/send_text_input_field.png", "CLICK INPUT")
 type("123")
-assertClick("1738945177975.png", "SEND TEXT INPUT BUTTON")
-waitVanish("1738946538905.png", WAIT_TIMEOUT)
-assertClick("1738945878928.png", "OK BUTTON")
-assertExists("1738947327463.png", "UNKNOWN DISCOUNT CODE TEXT EXIST")
-waitVanish("1738947327463.png", WAIT_TIMEOUT)
-find("1727790898506.png")
-assertClick("1727791300173.png", "CLICK DROPDOWN")
-assertExists("1738947549648.png", "INSERTED VALUE = 100 CZK")
-assertClick("1738947549648.png", "INSERT BANKNOTE 100 CZK")
+assertClick("tests/screenshots/send_text_input_button.png", "SEND TEXT INPUT BUTTON")
+waitVanish("tests/screenshots/enter_discount_code_text.png", WAIT_TIMEOUT)
+assertClick("tests/screenshots/OK_button.png", "OK BUTTON")
+assertExists(
+    "tests/screenshots/unknown_discount_code_text.png",
+    "UNKNOWN DISCOUNT CODE TEXT EXIST",
+)
+waitVanish("tests/screenshots/unknown_discount_code_text.png", WAIT_TIMEOUT)
+find("tests/screenshots/CZK_banknote_dropdown.png")
+assertClick("tests/screenshots/CZK_banknote_dropdown_button.png", "CLICK DROPDOWN")
+assertExists("tests/screenshots/100_banknote_value.png", "INSERTED VALUE = 100 CZK")
+assertClick("tests/screenshots/100_banknote_value.png", "INSERT BANKNOTE 100 CZK")
 
 
 def openBanknoteDropdown():
-    if has("1727792164614.png"):
-        assertClick("1738947549648.png", "SELECT 100 CZK")
+    if has("tests/screenshots/toggle_button.png"):
+        assertClick("tests/screenshots/100_banknote_value.png", "SELECT 100 CZK")
 
 
-assertClick("1738947729117.png", "INSERT BANKNOTE 100 CZK")
-assertExists("1738946287704.png", "INSERT CASH TEXT")
-wait("1738950216564.png", WAIT_TIMEOUT)
-assertClick("1738950216564.png", "BUY LTC BUTTON")
-wait("1738950590691.png", WAIT_TIMEOUT)
-assertExists("1738950590691.png", "PROCESSING TRANSACTION COMPLETED TEXT EXIST")
-assertClick("1738950606099.png", "DONE BUTTON")
+assertClick("tests/screenshots/insert_banknote_button.png", "INSERT BANKNOTE 100 CZK")
+assertExists("tests/screenshots/insert_cash_text.png", "INSERT CASH TEXT")
+wait("tests/screenshots/BUY_LTC_button.png", WAIT_TIMEOUT)
+assertClick("tests/screenshots/BUY_LTC_button.png", "BUY LTC BUTTON")
+wait("tests/screenshots/processing_transaction_completed_text.png", WAIT_TIMEOUT)
+assertExists(
+    "tests/screenshots/processing_transaction_completed_text.png",
+    "PROCESSING TRANSACTION COMPLETED TEXT EXIST",
+)
+assertClick("tests/screenshots/DONE_completed_button.png", "DONE BUTTON")
