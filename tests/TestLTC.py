4a7861e59fc59462d4c88c26ed79f20c8c3ef58c
Asem Saginayeva <asemsaginayeva@gmail.com>
2025-04-11 11:17:20 +0200

    update TestLTC.py (#23)

--- tests/TestLTC.py
+++ tests/TestLTC.py
@@ -76,6 +76,30 @@ class TestLTC(unittest.TestCase):
         )
         logging.info("=== Completed test: Unregistered Sell LTC ===")
 
+    def testRegisteredBuyLTC(self):
+        logging.info("=== Started test: Registered Buy LTC ===")
+        self.flow.performBuyFlow(tier="registered")
+        type(LTC_DESTINATION_ADDRESS)
+        self.screens.walletScreen.clickScanQrButton()
+        self.screens.walletScreen.insertBanknoteAndVerify("100 CZK")
+        self.screens.basePage.assertExists("BUY_LTC_button.png", "BUY LTC BUTTON")
+        self.screens.discountScreen.prepareDiscountDialog()
+        type(LTC_DISCOUNT_TEXT)
+        self.flow.completeDiscountFlow()
+        self.screens.insertMoneyScreen.buyLTC()
+        self.screens.marketingAgreementScreen.declineMarketingAgreement()
+        self.screens.dashboardScreen.completeTransaction()
+        logging.info("=== Completed test: Registered Buy LTC ===")
+
+    def testRegisteredSellLTC(self):
+        logging.info("=== Started test: Registered Sell LTC ===")
+        self.flow.performSellFlow(tier="registered")
+        type(LTC_DISCOUNT_TEXT)
+        self.flow.completeSellFlow(
+            useDiscount=True, requireMarketingDecline=True, requireSmsDismiss=False
+        )
+        logging.info("=== Completed test: Registered Sell LTC ===")
+
 
 if __name__ == "__main__":
     unittest.main(exit=False)
